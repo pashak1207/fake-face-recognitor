7c1f4a6a7d046d4c3ece20f9b5a33777b51993ea
Riki Ahmad Fadilah <riki@radyalabs.id>
2021-12-31 17:41:37 +0700

    rollback register

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -5,25 +5,7 @@ import fs from 'fs'
 
 class AuthController {
   static async register(req: Request, res: Response) {
-    const user = req.body.user
-    const image = req.body.photos
-
-    const data = image.replace(/^data:image\/\w+;base64,/, "")
-    const buf = Buffer.from(data, 'base64')
-    const fileName = user
-
-    fs.writeFile(`./images/${fileName}.jpg`, buf, async function (err : any) {
-        if (err) console.log(err)
-        // const imageInput = `./uploads/${fileName}.jpg`
-        // const recognize = new FaceRecognition(imageInput, user)
-
-        // const resultRecognize = await recognize.recognize()
-        
-        res.send(su)
-      })
-    }
-    // return res.send(req.file)
-
+    return res.send(req.file)
   }
 
   static async login(req: Request, res: Response) {
@@ -41,7 +23,7 @@ class AuthController {
       const recognize = new FaceRecognition(imageInput, user)
 
       const resultRecognize = await recognize.recognize()
-
+      
       res.send(resultRecognize)
     })
   }
