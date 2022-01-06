c6462d6dc84f4ff89997ad59166f2e704d16494e
sigit nurmajid <sigitnurmajid32@gmail.com>
2022-01-06 21:30:06 +0700

    handle error

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -9,23 +9,28 @@ class AuthController {
   }
 
   static async login(req: Request, res: Response) {
-    console.log('run')
-    const user = req.body.user
-    const image = req.body.photos
+    try {
+      console.log('run')
+      const user = req.body.user
+      const image = req.body.photos
+  
+      const data = image.replace(/^data:image\/\w+;base64,/, "")
+      const buf = Buffer.from(data, 'base64')
+      const fileName = user + '-' + nanoid()
+  
+      fs.writeFile(`./uploads/${fileName}.jpg`, buf, async function (err : any) {
+        if (err) console.log(err)
+        const imageInput = `./uploads/${fileName}.jpg`
+        const recognize = new FaceRecognition(imageInput, user)
+  
+        const resultRecognize = await recognize.recognize()
+        
+        res.send(resultRecognize)
+      })
+    } catch (error: any) {
+      res.status(500).send(error.message)
+    }
 
-    const data = image.replace(/^data:image\/\w+;base64,/, "")
-    const buf = Buffer.from(data, 'base64')
-    const fileName = user + '-' + nanoid()
-
-    fs.writeFile(`./uploads/${fileName}.jpg`, buf, async function (err : any) {
-      if (err) console.log(err)
-      const imageInput = `./uploads/${fileName}.jpg`
-      const recognize = new FaceRecognition(imageInput, user)
-
-      const resultRecognize = await recognize.recognize()
-      
-      res.send(resultRecognize)
-    })
   }
 }
 
